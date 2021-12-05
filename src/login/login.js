ce35ece300c6f714f0bfd6f8e85236ac33c35140
olek-arsee <olboygg@gmail.com>
2021-12-05 12:46:51 +0100

    inputs style and password visibility

--- src/login/login.js
+++ src/login/login.js
@@ -1,3 +1,35 @@
 function exit() {
     location.href = "../start-page/start.html"
 }
+
+document.getElementById("login").addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      document.getElementById("password").focus()
+    }
+});
+
+document.getElementById("password").addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      signIn();
+    }
+});
+
+function passwordVisibility() {
+    var input = document.getElementById("password");
+    if (input.type === "password") {
+      input.type = "text";
+      document.getElementById("not-visible").style.display = "none"
+      document.getElementById("visible").style.display = "block"
+    } else {
+      input.type = "password";
+      document.getElementById("visible").style.display = "none"
+      document.getElementById("not-visible").style.display = "block"
+    }
+}
+
+function signIn() {
+    let login = document.getElementById("login").value
+    let password = document.getElementById("password").value
+
+    console.log(login+password)
+}
