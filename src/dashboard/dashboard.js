549cdb1929e040073b3c1f82bc3708fdbb73059b
olek-arsee <olboygg@gmail.com>
2021-12-21 17:15:48 +0100

    list todos - backend implementation

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,22 +1,26 @@
 if (navigator.language == "pl") {
-  document.getElementById("search-bar").placeholder = `Szukaj TODO`
+  document.getElementById("search-bar").placeholder = `Szukaj TODO`;
   document.getElementById("hi").innerHTML = `Cześć`;
   document.getElementById("text-add").innerHTML = `Dodaj nowe zadanie!`;
 } else {
-  document.getElementById("search-bar").placeholder = `Search for TODO`
+  document.getElementById("search-bar").placeholder = `Search for TODO`;
   document.getElementById("hi").innerHTML = `Hi`;
   document.getElementById("text-add").innerHTML = `Add new task!`;
 }
+
+const preferences = require("../../preferences.json");
+document.getElementById("name").innerHTML = preferences.user.firstname;
+loadTodos();
 
 function search() {
   let searchInput = document.getElementById("search-bar");
 
   if (searchInput.style.display === "inline-block") {
-    document.getElementById("search-bar").classList = "slideOutRight"
-    document.getElementById("search-bar").animationPlayState = "running"
+    document.getElementById("search-bar").classList = "slideOutRight";
+    document.getElementById("search-bar").animationPlayState = "running";
     sleep(400).then(() => {
-        searchInput.style.display = "none";
-    })
+      searchInput.style.display = "none";
+    });
   } else {
     document.getElementById("search-bar").classList = "slideInRight";
     searchInput.style.display = "inline-block";
@@ -24,12 +28,35 @@ function search() {
 }
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
-const preferences = require("../../preferences.json");
-document.getElementById("name").innerHTML = preferences.user.firstname;
 
 if (!document.getElementById("name").innerHTML) {
   document.getElementById("hi").style.fontWeight = 500;
+}
+
+async function loadTodos() {
+  await fetch("https://justy-backend.herokuapp.com/todos", {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + preferences.token,
+    },
+  })
+    .then(async (response) => {
+      if (response.ok) {
+        return response.json().then(function (json) {
+          const todos = JSON.parse(json.list);
+          for (const todo of todos) {
+            console.log(todo)
+          }
+        });
+      } else {
+        console.log("something went wrong");
+      }
+    })
+    .catch((reason) => {
+      console.log("something went wrong");
+    });
 }
